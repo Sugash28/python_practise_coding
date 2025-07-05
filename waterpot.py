262bbc394444b844c19a13fc144ed7b5d19adf23
sug <sugashsugu@gmail.com>
2025-07-05 20:02:25 +0530

    new problem

--- waterpot.py
+++ waterpot.py
@@ -1,2 +1,20 @@
-a = [1,8,6,2,5,4,8,3,7]
+a =[4,3,2,1,4]
 
+left=0
+right=len(a)-1
+max_area=0
+
+while left<right:
+    width=right-left
+    ht=min(a[left],a[right])
+    area=width*ht
+    max_area=max(max_area,area)
+    if a[left]<a[right]:
+        left+=1
+    else:
+        right-=1
+        
+print( max_area)
+        
+
+    
